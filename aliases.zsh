37cdc33fe2cb498cf4d80fee56566d9e6cb20ac5
paulj <jaffrepaul@gmail.com>
2018-06-18 22:20:08 -0400

    added aliases

---
+++ aliases.zsh
@@ -0,0 +1,36 @@
+# Shortcuts
+alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias reloadcli="source $HOME/.zshrc"
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+alias library="cd $HOME/Library"
+alias sites="cd $HOME/Sites"
+alias root="cd $HOME/"
+
+# Git
+alias gs="git status"
+alias gaa='git add --all'
+alias gd="git diff"
+alias branch="git check out -b"
+alias gl="git log --oneline --decorate --color"
+
+# zsh
+alias zshconfig="code ~/.zshrc"
+alias ohmyzsh="code ~/.oh-my-zsh"
+
+# Vagrant
+# alias v="vagrant global-status"
+# alias vup="vagrant up"
+# alias vhalt="vagrant halt"
+# alias vssh="vagrant ssh"
+# alias vreload="vagrant reload"
+# alias vrebuild="vagrant destroy --force && vagrant up"
+
+# Docker
+# alias dstop="docker stop $(docker ps -a -q)"
+# alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
+# alias dpurgeimages="docker rmi $(docker images -q)"
+# dbuild() { docker build -t=$1 .; }
+# dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
